1b6881eeac8cd2d846f0322bc6c8143b183115ad
unknown <David@DESKTOP-David.fortitude.local>
2020-10-22 21:25:56 -0400

    button to undo last change

--- assets/scripts/genres.js
+++ assets/scripts/genres.js
@@ -1,19 +1,21 @@
 $(document).ready(function() {
     var genres = ['rock','country','rap','R&B','Pop','Reggae'];
-    var maxedOut = []
+    var maxedOut = [];
     //max counter per button
     var maxClicks = 5;
     var genreMaxClicks = 3;
+    var lastClick;
 
     var musicPref = [];
+    var prefIndex = 0;
 
 
     genres.forEach( e => {
         var button = $('<div>');
-        button.text(e)
-        button.addClass('click')
-        button.attr('data-genre',e)
-        button.attr('id',e)
+        button.text(e);
+        button.addClass('click');
+        button.attr('data-genre',e);
+        button.attr('id',e);
         $('#buttonDivs').append(button);
     });
 
@@ -23,17 +25,39 @@ $(document).ready(function() {
         var genre = $(this).attr('data-genre')
         // function to handle data storage
         if (maxedOut.indexOf(genre) == -1) {
+            lastClick = genre;
             trackPicks(genre);
             
             // $('#' + genre).
 
             // function to handled the css
         }
-        else {
-            // undo previous click
+        // function to see if total was hit 
+    })
+
+
+    // function to undo
+    $('#undo').on('click',function(){
+
+        function isPicked(picked) { 
+            return picked.genre === lastClick;
         }
-        
-        // function to see if total was hit
+
+        var picked = musicPref.find(isPicked);
+        var i = picked.index
+
+        musicPref[i].count--;
+
+        if(musicPref[i].count == 0) {
+            musicPref.splice(i,1)
+            prefIndex--;
+        }
+    })
+
+    $('#reset').on('click',function(){
+        musicPref.length = 0
+        prefIndex = 0
+        maxedOut.length = 0
     })
 
 
@@ -52,13 +76,13 @@ $(document).ready(function() {
             musicPref.push(
                 {
                     genre: genre,
-                    count: 1
+                    count: 1,
+                    index: prefIndex
                 }
             )
-
+            prefIndex++;
         }
         else {
-            console.log(musicPref)
             picked.count ++;
             if (picked.count == 3) {
                 maxedOut.push(genre)
@@ -67,5 +91,3 @@ $(document).ready(function() {
         
     }
 });
-
-
