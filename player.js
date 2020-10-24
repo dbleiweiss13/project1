0b7e157d7812f285ef422f3abe20c55907353ef8
Maryam Chaudhry <maryamc1798@gmail.com>
2020-10-24 10:36:41 -0400

    changing js file

--- player.js
+++ player.js
@@ -1,92 +1,12 @@
-// DONT DO ANYTHING WITH THIS OR USE ANY VARIABLES FROM THIS YET @ Backend team
-// $(document).ready(function() {
-function calculateTotalValue(length) {
-    var minutes = Math.floor(length / 60),
-      seconds_int = length - minutes * 60,
-      seconds_str = seconds_int.toString(),
-      seconds = seconds_str.substr(0, 2),
-      time = minutes + ':' + seconds
-  
-    return time;
-  }
-  
-  function calculateCurrentValue(currentTime) {
-    var current_hour = parseInt(currentTime / 3600) % 24,
-      current_minute = parseInt(currentTime / 60) % 60,
-      current_seconds_long = currentTime % 60,
-      current_seconds = current_seconds_long.toFixed(),
-      current_time = (current_minute < 10 ? "0" + current_minute : current_minute) + ":" + (current_seconds < 10 ? "0" + current_seconds : current_seconds);
-  
-    return current_time;
-  }
-  // some of these dont work lol
-  function initProgressBar() {
-    var player = document.getElementById('player');
-    var length = player.duration
-    var current_time = player.currentTime;
-  
-    // calculate total length of value
-    var totalLength = calculateTotalValue(length)
-    $(".end-time").html(totalLength);
-  
-    // calculate current value time
-    var currentTime = calculateCurrentValue(current_time);
-    $(".start-time").html(currentTime);
-  
-    var progressbar = document.getElementById('seekObj');
-    progressbar.value = (player.currentTime / player.duration);
-    progressbar.addEventListener("click", seek);
-  
-    if (player.currentTime == player.duration) {
-      $('#play-btn').removeClass('pause');
-    }
-  
-    function seek(evt) {
-      var percent = evt.offsetX / this.offsetWidth;
-      player.currentTime = percent * player.duration;
-      progressbar.value = percent / 100;
-    }
-  };
-  
-  function initPlayers(num) {
-    // pass num in if there are multiple audio players e.g 'player' + i
-  
-    for (var i = 0; i < num; i++) {
-      (function() {
-  
-        // Variables
-        // ----------------------------------------------------------
-        // audio embed object
-        var playerContainer = document.getElementById('player-container'),
-          player = document.getElementById('player'),
-          isPlaying = false,
-          playBtn = document.getElementById('play-btn');
-  
-        // Controls Listeners
-        // ----------------------------------------------------------
-        if (playBtn != null) {
-          playBtn.addEventListener('click', function() {
-            togglePlay()
-          });
-        }
-  
-        // Controls & Sounds Methods
-        // ----------------------------------------------------------
-        function togglePlay() {
-          if (player.paused === false) {
-            player.pause();
-            isPlaying = false;
-            $('#play-btn').removeClass('pause');
-  
-          } else {
-            player.play();
-            $('#play-btn').addClass('pause');
-            isPlaying = true;
-          }
-        }
-      }());
-    }
-  }
- 
-  initPlayers(jQuery('#player-container').length);
-// }); 
+(function($) {
+  $(document).ready(function() {
+
+      // Pause/Play functionality
+      var playButton = $('.control-play'),
+          album = $('.album');
+
+      playButton.on('click', function() {
+          $('.music-player-container').toggleClass('is-playing');
+      });
+  });
+})(jQuery);
