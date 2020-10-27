89d19dc9c1a51b61c8bfa71543489b2975ce76cc
Maryam Chaudhry <maryamc1798@gmail.com>
2020-10-27 19:52:39 -0400

    play pause working

--- player.js
+++ player.js
@@ -1,14 +1,43 @@
+$(document).ready(function() {
+    // play song function 
+    // function play() {
+    //     var playButton = $("#playBtn");
 
-// (function($) {
-//   $(document).ready(function() {
-//       // Pause/Play functionality
-//       var playButton = $('.control-play'),
-//           album = $('.album');
-//       playButton.on('click', function() {
-//           $('.music-player-container').toggleClass('is-playing');
-//       });
-//   });
-// })(jQuery);
+    // }
+    var playButton = $('#playBtn');
+    var pauseButton = $("#pauseBtn");
+    
+    playButton.on('click', function() {
+        playButton.addClass("hide");
+        pauseButton.removeClass("hide");
+        //function to play song
+    });
+
+    pauseButton.on("click", function(){
+        pauseButton.addClass("hide");
+        playButton.removeClass("hide");
+        // function to pause song
+    }); 
+
+
+
+
+      // fast forward functionality
+    //   var fastButton = $("#fastBtn");
+
+    //   $(".music-container").toggleClass("")
+
+    // favorite button
+    var favButton = $("#favBtn");
+
+    favButton.on("click", function(){
+        
+    });
+
+
+
+  });
+
 
 
 
