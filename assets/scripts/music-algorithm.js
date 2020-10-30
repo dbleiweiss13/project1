7a3531ad7e20dc6b60b1f8099b3cd9b4d03c829f
David Bleiweiss <dbleiweiss123@gmail.com>
2020-10-30 08:15:21 -0700

    pulled from master and updated api key

--- assets/scripts/music-algorithm.js
+++ assets/scripts/music-algorithm.js
@@ -3,7 +3,7 @@ var googleReady = false;
 var spodifyReady = false;
 
 // access token fir spodify
-var spodifyAccessToken = "BQC5k9_RcV5u5pIYVXT1Utwe-qiH3dg6wcUT6jqBHdOh-ffaXZqz86vVUkqW0eDfl6GqiLd1yG0sxqDf0gGL3QgFeahLOLJBU1zbMJeyVN7ECSrmsz6oht5rx1C1vGdIpGjTpe5ZJIucBSqOikfxDQhMO-8RBbM";
+var spodifyAccessToken = "BQC8BGpC_DxbHmu7RV9NwCFuK_pc3z9AzW9U3mI1K_r6952xIAZ4LhLE_cW0B6lARnZ1swwKL2kA17anEt_iFWKnY_VSOViY3OS08mwKlVSEjkRYiaa9nzx5Iotv1NrE2ZdixtEzXWJR4fNy3wlomEstnXLGkgs";
 
 //google keys
 var googleApikey = 'AIzaSyD_Lxn97l1Pe7HVXohJPIojqhqHyuCevF4';
@@ -182,9 +182,9 @@ function getSong(artist) {
                             console.log(data.tracks[trackSelect])
                             spodifyReady = true;
                             songName = trackSelect.name;
-                            artistName = trackSelect.album.artists[0].name;
-
                             console.log(songName);
+
+                            artistName = trackSelect.album.artists[0].name;
                             console.log(artistName);
 
                             getVideo ()
